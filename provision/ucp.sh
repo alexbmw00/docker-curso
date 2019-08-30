c362006229b0ec72f434bef1ec9d1c84b3c702a3
Hector Vido <hector_vido@yahoo.com.br>
2019-08-30 18:30:25 -0300

    Modificado valores de provisionamento

--- provision/ucp.sh
+++ provision/ucp.sh
@@ -7,4 +7,9 @@ docker container run --rm -it --name ucp \
 docker/ucp:3.2.0 install \
 --force-minimums \
 --host-address 27.11.90.10 \
+--admin-username admin \
+--admin-password 4linux123 \
+--disable-tracking \
+--disable-usage \
+--pod-cidr 100.14.0.0/16 \
 --interactive
