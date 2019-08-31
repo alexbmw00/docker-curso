bd5d9ecd8e500bffbeee3ac96c059e366902c059
Hector Vido <39673799+hector-vido@users.noreply.github.com>
2019-08-31 12:33:19 -0300

    Adicionado o dtr.sh

--- provision/dtr.sh
+++ provision/dtr.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-
+docker run -it --rm docker/dtr install \
+--dtr-external-url https://dtr.27-11-90-20.nip.io \
+--ucp-node docker2 \
+--ucp-username admin \
+--ucp-url https://docker1.27-11-90-10.nip.io \
+--ucp-insecure-tls
